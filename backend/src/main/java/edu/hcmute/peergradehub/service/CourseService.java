f9349c0dca804533ccb40a970300485c350255c1
nhu1711 <trannhu1711tt@gmail.com>
2026-06-28 23:04:27 +0700

    feat(course): add getActiveCourses method to service interface

--- backend/src/main/java/edu/hcmute/peergradehub/service/CourseService.java
+++ backend/src/main/java/edu/hcmute/peergradehub/service/CourseService.java
@@ -1,13 +1,13 @@
 package edu.hcmute.peergradehub.service;
 
+import java.util.List;
+
 import edu.hcmute.peergradehub.dto.request.course.CreateCourseRequest;
 import edu.hcmute.peergradehub.dto.request.course.UpdateCourseRequest;
 import edu.hcmute.peergradehub.dto.response.course.CourseSummaryResponse;
 import edu.hcmute.peergradehub.dto.response.course.CourseWorkspaceResponse;
 import edu.hcmute.peergradehub.entity.Course;
 import edu.hcmute.peergradehub.entity.User;
-
-import java.util.List;
 
 public interface CourseService {
     Course createCourse(String courseName, String classCode, String semester, User lecturer, String description);
@@ -16,4 +16,5 @@ public interface CourseService {
     CourseWorkspaceResponse createCourse(CreateCourseRequest request, Long actorId);
     CourseWorkspaceResponse getCourseWorkspace(Long courseId, Long actorId);
     CourseWorkspaceResponse updateCourse(Long courseId, UpdateCourseRequest request, Long actorId);
+    List<CourseSummaryResponse> getActiveCourses(Long actorId);
 }
