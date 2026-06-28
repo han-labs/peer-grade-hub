31becc363fe280c9b22ad41b2017df9948531e36
nhu1711 <trannhu1711tt@gmail.com>
2026-06-28 23:04:07 +0700

    feat(course): add method to find courses by lecturer and status

--- backend/src/main/java/edu/hcmute/peergradehub/dao/CourseDao.java
+++ backend/src/main/java/edu/hcmute/peergradehub/dao/CourseDao.java
@@ -1,11 +1,13 @@
 package edu.hcmute.peergradehub.dao;
 
-import edu.hcmute.peergradehub.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+ 
+import edu.hcmute.peergradehub.entity.Course;
+import edu.hcmute.peergradehub.enumeration.CourseStatus;
 
 @Repository
 public interface CourseDao extends JpaRepository<Course, Long> {
@@ -15,4 +17,5 @@ public interface CourseDao extends JpaRepository<Course, Long> {
     Optional<Course> findByIdAndLecturerId(Long courseId, Long lecturerId);
     boolean existsByClassCodeAndIdNot(String classCode, Long courseId);
     boolean existsByInvitationCode(String invitationCode);
+    List<Course> findByLecturerIdAndCourseStatus(Long lecturerId, CourseStatus status);
 }
